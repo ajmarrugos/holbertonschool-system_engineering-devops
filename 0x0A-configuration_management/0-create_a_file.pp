b267bbb842326e168c1050aaf2358d0372f4ccf0
ajmarrugos <ajmarrugos@gmail.com>
2022-08-19 21:41:40 -0500

    Puppet manifests

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, create a file in /tmp.
+
+file { '0-create_a_file.pp':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# Using Puppet, install puppet-lint.
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
+  source   => 'https://rubygems.org',
+}
