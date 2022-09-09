439288b9da6cf71f7a4c23269cd48da32e414976
ajmarrugos <ajmarrugos@gmail.com>
2022-09-09 11:41:51 -0500

    Gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Python Script to gather data from API """
+import requests
+from sys import argv
+
+
+todos_url = "https://jsonplaceholder.typicode.com/todos/"
+users_url = "https://jsonplaceholder.typicode.com/users/"
+id = argv[1]
+
+name = requests.get(users_url + id).json()
+task = requests.get(todos_url, params={"userId": id}).json()
+total_task = requests.get(
+    todos_url, params={"userId": id, "completed": "true"}).json()
+
+print("Employee {} is done with task ({}/{}):".format(name.get("name"),
+      len(total_task), len(task)))
+
+for task in total_task:
+    print("\t {}".format(task.get("title")))
