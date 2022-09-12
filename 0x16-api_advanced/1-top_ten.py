8b983d6517861cdc4da126652208e4478d0cd958
ajmarrugos <ajmarrugos@gmail.com>
2022-09-11 23:31:32 -0500

    Ask API for Top 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Prints titles of first 10 hot posts listed"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit"""
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    user_agent = "reddit_user"
+
+    if after:
+        url += "?after={}".format(after)
+    headers = {"User-Agent": user_agent}
+    request = requests.get(url, headers=headers, allow_redirects=False)
+
+    if request.status_code != 200:
+        return None
+    data = request.json()["data"]
+    posts = data["children"]
+    for post in posts:
+        count += 1
+        hot_list.append(post["data"]["title"])
+
+    after = data["after"]
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    else:
+        return hot_list
